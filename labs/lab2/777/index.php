790e7f77cbd071b6f2f05fe9daa23215e7bfb9c6
eirkrw <ering-walters@csumb.edu>
2017-09-06 20:17:58 +0000

    have page working in html and php alongside one another.

---
+++ labs/lab2/777/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> 777 Slot Machine </title>
+    </head>
+    <body>
+        
+        <?php
+        $symbol7 = "seven";
+        
+        echo "<img src='img/$symbol7.png' alt='$symbol7' title='$symbol7' width='70' />";
+        
+        ?>
+        <img src="img/cherry.png" alt="Cherry" title="Cherry" width="70" />
+        <img src="img/lemon.png" alt="Lemon" title="Lemon" width="70"/>
+        <img src="img/.png" alt="" title="" width="70"/>
+    </body>
+</html>
