df7738582d01593ff1a95ea157a2b71967cb23ea
eirkrw <ering-walters@csumb.edu>
2017-10-11 04:29:06 +0000

    quiz format looks good

--- cst336/homework/homework3/index.php
+++ cst336/homework/homework3/index.php
@@ -15,126 +15,206 @@
 <body>
     <div class="col-md-2"></div>
     <div id="content" class="col-md-8">
+        <div id="pad">
     <h1> Pasta Quiz! </h1>
     <h2>Take the quiz to find out which pasta noodle you are!</h2>
+    Please enter your name: <input type="text" name="name"></input>
     <h3>
-        Q1: A stranger drops their bag and all their papers fell out. what is 
+        Q1: A stranger drops their bag and all their papers fall out. what is 
         your reaction?
     </h3>
     <ul>
-        <li>Help them pick it up.</li>
-        <li>Use this time to get their number and ask them out on a date.</li>
-        <li>Keep walking and as soon as you get home, sob to your best friend 
+         <div class="radio">
+         <input class="bowtie" type= "radio" id= "lhorizontal" name="q1" value = "horizontal">Help them pick it up.</input><br/>
+         <input class="linguini" type= "radio" id= "lhorizontal" name="q1" value = "horizontal">Use this time to get their number and ask them out on a date.</input><br/>
+         <input class="spaghetti" type= "radio" id= "lhorizontal" name="q1" value = "horizontal">Keep walking and as soon as you get home, sob to your best friend 
         about how you had the opportunity to make the world a little bit of a 
-        better place, but you how failed that poor stranger.</li>
-        <li>Call 911.</li>
-        <li>Laugh with them, but not at them, then buy them coffee for their 
-        hardships.</li>
+        better place, but you how failed that poor stranger.</input><br/>
+         <input class="pene" type= "radio" id= "lhorizontal" name="q1" value = "horizontal">Call 911.</input><br/>
+         <input class="spiral" type= "radio" id= "lhorizontal" name="q1" value = "horizontal">Laugh with them, but not at them, then buy them coffee for their 
+        hardships.</input><br/>
+         </div>
+
+        
+        
+        
+        
+        
+       
     </ul>
     <h3>
         Q2: A friend invites you to go skydiving. You do you respond?
     </h3>
     
     <ul>
-        <li>Excitedly accept the offer.</li>
-        <li>Reply with statistics on sharks and how many people die each
-        year due to shark diving accidents.</li>
-        <li>Make up an event as an excuse.</li>
-        <li>Sprint the other direction.</li>
-        <li>Ask if you can bring your homemade parachute and whether your 
-        mom can tag along.</li>
+        <div class="radio">
+
+         <input class="bowtie" type= "radio" id= "lhorizontal" name="q2" value = "horizontal">Excitedly accept the offer.</input><br/>
+         <input class="linguini" type= "radio" id= "lhorizontal" name="q2" value = "horizontal">Reply with statistics on sharks and how many people die each
+        year due to shark diving accidents.</input><br/>
+         <input class="spaghetti" type= "radio" id= "lhorizontal" name="q2" value = "horizontal">Make up an event as an excuse.</input><br/>
+         <input class="pene" type= "radio" id= "lhorizontal" name="q2" value = "horizontal">Sprint the other direction.</input><br/>
+         <input class="spiral" type= "radio" id= "lhorizontal" name="q2" value = "horizontal">Ask if you can bring your homemade parachute and whether your 
+        mom can tag along.</input><br/>
+         </div>
+
+        
+        
+        
+        
+        
+        
+
     </ul>
     <h3>
         Q3: Someone walks by you and calls you a dweeb. How do you react?
     </h3>
     <ul>
-        <li>Tell them to have a good day and express your hope that their 
-        insecurities are resolved.</li>
-        <li>Lecture them social equality and recommend them a resource on
-        progressive communication.</li>
-        <li>Respond by letting them know the color of your belt in the
-        martial art you've been practicing.</li>
-        <li>Cry.</li>
-        <li>Repress until you get home to your dog then project your emotions
-        by calling him/her a dweeb</li>
+        <div class="radio">
+
+         <input class="bowtie" type= "radio" id= "lhorizontal" name="q3" value = "horizontal">Tell them to have a good day and express your hope that their 
+        insecurities are resolved.</input><br/>
+         <input class="linguini" type= "radio" id= "lhorizontal" name="q3" value = "horizontal">Lecture them social equality and recommend them a resource on
+        progressive communication.</input><br/>
+         <input class="spaghetti" type= "radio" id= "lhorizontal" name="q3" value = "horizontal">Respond by letting them know the color of your belt in the
+        martial art you've been practicing.</input><br/>
+         <input class="pene" type= "radio" id= "lhorizontal" name="q3" value = "horizontal">Cry.</input><br/>
+         <input class="spiral" type= "radio" id= "lhorizontal" name="q3" value = "horizontal">Repress until you get home to your dog then project your emotions
+        by calling him/her a dweeb</input><br/>
+         </div>
+        
+        
+        
+        
+        
     </ul>
      <h3>
         Q4: You find a wallet ont a restaurant floor. How do you handle it?
     </h3>
     <ul>
-        <li>Bring it to the manager and make sure it's in good hands before you 
-        leave.</li>
-        <li>Find the person using their identification and scold them for their 
-        lack of responsibility.</li>
-        <li>Use the cash inside to buy your friends a round of drinks.</li>
-        <li>Pocket the wallet and quietly walk out.</li>
-        <li>Inform the nearest person that you are thinking of a number between 
+        <div class="radio">
+
+         <input class="bowtie" type= "radio" id= "lhorizontal" name="q4" value = "horizontal">Bring it to the manager and make sure it's in good hands before you 
+        leave.</input><br/>
+         <input class="linguini" type= "radio" id= "lhorizontal" name="q4" value = "horizontal">Find the person using their identification and scold them for their 
+        lack of responsibility.</input><br/>
+         <input class="spaghetti" type= "radio" id= "lhorizontal" name="q4" value = "horizontal">Use the cash inside to buy your friends a round of drinks.</input><br/>
+         <input class="pene" type= "radio" id= "lhorizontal" name="q4" value = "horizontal">Pocket the wallet and quietly walk out.</input><br/>
+         <input class="spiral" type= "radio" id= "lhorizontal" name="q4" value = "horizontal">Inform the nearest person that you are thinking of a number between 
         1 and 100 (inclusive). If they're able to guess your middle name, let 
-        them keep the found wallet.</li>
+        them keep the found wallet.</input><br/>
+         </div>
+        
+        
+        
+        
+        
         
     </ul>
      <h3>
         Q5: A friend offers you Xxtra Flamin' Hot Cheetos. What do you do?
     </h3>
     <ul>
-        <li>Politely accept a few and enjoy the flavor.</li>
-        <li>Rudely inform him of how bad they are for them and how immoral the 
-        manufacturing is.</li>
-        <li>Take far more than cheetos than they were offering.</li>
-        <li>Complain about how you don't like spicy food and scrunch your face 
-        at the thought.</li>
-        <li>Swat the bag out of their hand and claim that it is for their health, 
+        <div class="radio">
+
+         <input class="bowtie" type= "radio" id= "lhorizontal" name="q5" value = "horizontal">Politely accept a few and enjoy the flavor.</input><br/>
+         <input class="linguini" type= "radio" id= "lhorizontal" name="q5" value = "horizontal">Rudely inform him of how bad they are for them and how immoral the 
+        manufacturing is.</input><br/>
+         <input class="spaghetti" type= "radio" id= "lhorizontal" name="q5" value = "horizontal">Take far more than cheetos than they were offering.</input><br/>
+         <input class="pene" type= "radio" id= "lhorizontal" name="q5" value = "horizontal">Complain about how you don't like spicy food and scrunch your face 
+        at the thought.</input><br/>
+         <input class="spiral" type= "radio" id= "lhorizontal" name="q5" value = "horizontal">Swat the bag out of their hand and claim that it is for their health, 
         even though you know that you are asserting a subconscious dominance 
-        over them.</li>
+        over them.</input><br/>
+         </div>
+        
+        
+        
+        
+        
     </ul>
+    
     <h3>Q6: You have to pick your child up from daycare, but are in the middle of 
     an important project at work. What do you do?</h3>
     <ul>
-    <li>Inform your boss and make the decision based on their understanding.</li>
-    <li>Make sure you are on time to pick up your child.</li>
-    <li>Assume your child will be fine for a couple more hours.</li>
-    <li>Tell your child to walk home. They can handle it.</li>
-    <li>Ask a stranger to pick up your kid. Before the stranger gets there, call 
-    your child and give him self-defense tips.</li>
+        <div class="radio">
+
+         <input class="bowtie" type= "radio" id= "lhorizontal" name="q6" value = "horizontal">Inform your boss and make the decision based on their understanding.</input><br/>
+         <input class="linguini" type= "radio" id= "lhorizontal" name="q6" value = "horizontal">Make sure you are on time to pick up your child.</input><br/>
+         <input class="spaghetti" type= "radio" id= "lhorizontal" name="q6" value = "horizontal">Assume your child will be fine for a couple more hours.</input><br/>
+         <input class="pene" type= "radio" id= "lhorizontal" name="q6" value = "horizontal">Tell your child to walk home. They can handle it.</input><br/>
+         <input class="spiral" type= "radio" id= "lhorizontal" name="q6" value = "horizontal">Ask a stranger to pick up your kid. Before the stranger gets there, call 
+    your child and give him self-defense tips.</input><br/>
+         </div>
+    
+    
+    
+    
+    
     </ul>
+    
      <h3>Q7: An alien asks you to take him to your leader. How do you react?</h3>
     <ul>
-    <li>Give him accurate directions to your boss and ask if there's any 
-    other way you can help.</li>
-    <li>Inform the alien that you are strong independent person with a free
-    spirit and do not need a leader.</li>
-    <li>Pretend to lead the alien somewhere he wants, but secretly organize a 
-    trap to bring him under law enforcement.</li>
-    <li>Play dead.</li>
-    <li>Attempt to convince the alien to have dinner at your house, then build 
-    LEGO structures with the alien, later that night.</li>
+        <div class="radio">
+
+         <input class="bowtie" type= "radio" id= "lhorizontal" name="q7" value = "horizontal">Give him accurate directions to your boss and ask if there's any 
+    other way you can help.</input><br/>
+         <input class="linguini" type= "radio" id= "lhorizontal" name="q7" value = "horizontal">Inform the alien that you are strong independent person with a free
+    spirit and do not need a leader.</input><br/>
+         <input class="spaghetti" type= "radio" id= "lhorizontal" name="q7" value = "horizontal">Pretend to lead the alien somewhere he wants, but secretly organize a 
+    trap to bring him under law enforcement.</input><br/>
+         <input class="pene" type= "radio" id= "lhorizontal" name="q7" value = "horizontal">Play dead.</input><br/>
+         <input class="spiral" type= "radio" id= "lhorizontal" name="q7" value = "horizontal">Attempt to convince the alien to have dinner at your house, then build 
+    LEGO structures with the alien, later that night.</input><br/>
+         </div>
+    
+    
+    
+    
+    
     </ul>
      <h3>Q8: Your friend trips on a skateboard. How do you handle it?</h3>
     <ul>
-    <li>You whip out your Neosporin and kiss any boo-boos better, then write a 
-    thoughtful sentiment in a Get-Well-Soon card.</li>
-    <li>Use this as a lesson on the dangers of skating.</li>
-    <li>Laugh.</li>
-    <li>Wear a helmet from now on, even though you don't skate.</li>
-    <li>Fall in the same way so your friend doesn't feel alone, then whip open 
-    a bag of chips on the ground to share with your friend.</li>
+        <div class="radio">
+
+         <input class="bowtie" type= "radio" id= "lhorizontal" name="q8" value = "horizontal">You whip out your Neosporin and kiss any boo-boos better, then write a 
+    thoughtful sentiment in a Get-Well-Soon card.</input><br/>
+         <input class="linguini" type= "radio" id= "lhorizontal" name="q8" value = "horizontal">Use this as a lesson on the dangers of skating.</input><br/>
+         <input class="spaghetti" type= "radio" id= "lhorizontal" name="q8" value = "horizontal">Laugh.</input><br/>
+         <input class="pene" type= "radio" id= "lhorizontal" name="q8" value = "horizontal">Wear a helmet from now on, even though you don't skate.</input><br/>
+         <input class="spiral" type= "radio" id= "lhorizontal" name="q8" value = "horizontal">Fall in the same way so your friend doesn't feel alone, then whip open 
+    a bag of chips on the ground to share with your friend.</input><br/>
+         </div>
+    
+    
+    
+    
+    
     </ul>
     <h3>Q9: You failed an exam that you studied hours for.</h3>
     <ul>
-        <li>Accept your defeat, make a note to study better next time, and make 
-        sure you learn what you previously did not.</li>
-        <li>Build a case on why you deserve those points then present it to the 
-        teacher.</li>
-        <li>Call your teacher a mean name to your friend, then play video games 
-        to get your mind off it.</li>
-        <li>Call your parents and apologize to them.</li>
-        <li></li>
+        <div class="radio">
+
+         <input class="bowtie" type= "radio" id= "lhorizontal" name="q9" value = "horizontal">Accept your defeat, make a note to study better next time, and make 
+        sure you learn what you previously did not.</input><br/>
+         <input class="linguini" type= "radio" id= "lhorizontal" name="q9" value = "horizontal">Build a case on why you deserve those points then present it to the 
+        teacher.</input><br/>
+         <input class="spaghetti" type= "radio" id= "lhorizontal" name="q9" value = "horizontal">Call your teacher a mean name to your friend, then play video games 
+        to get your mind off it.</input><br/>
+         <input class="pene" type= "radio" id= "lhorizontal" name="q9" value = "horizontal">Call your parents and apologize to them.</input><br/>
+         <input class="spiral" type= "radio" id= "lhorizontal" name="q9" value = "horizontal"></input><br/>
+         </div>
+        
+        
+        
+        
+        
     </ul>
     
     <h3>Q10: How many punches can you take to the throat?</h3>
     <!--Number thing goes here. -->
-    
+    <input type="number" name="q10"></input>
     </div>
-    
+    </div>
     <div class="col-md-2"></div> 
 </body>
