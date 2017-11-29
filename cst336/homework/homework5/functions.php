50d7af50d317b62476c57913eef9ea4d2c7c4103
eirkrw <ering-walters@csumb.edu>
2017-11-29 10:11:20 +0000

    significant sql progress

--- cst336/homework/homework5/functions.php
+++ cst336/homework/homework5/functions.php
@@ -2,19 +2,39 @@
 include '../../dbConnection.php';
 $conn = getDatabaseConnection();
 
-
-$sql = "SELECT *
-        FROM tc_product
-        WHERE Asin = :asin"; 
+//Inserting into history
+$sql = "INSERT INTO productHistory (Asin)
+        VALUES (:Asin)";
+         
         
 $namedParameters = array();
-$namedParameters[':Asin'] = $_GET['asin'];
+$namedParameters[':Asin'] =   $_GET['asin'] ;
+
+print_r($namedParameters);
 
 $stmt = $conn->prepare($sql);
 $stmt->execute($namedParameters);
-$record = $stmt->fetch(PDO::FETCH_ASSOC);//expecting only one record
 
-print_r($record);
+$sql = "SELECT *
+        FROM tc_product
+        WHERE 1
+        AND Asin LIKE :Asin";
+         
+        
+$namedParameters = array();
+$namedParameters[':Asin'] = "%" . $_GET['asin'] . "%";
 
-echo json_encode($record);
+ //print_r($namedParameters);
+
+// $stmt = $conn->prepare($sql);
+// $stmt->execute($namedParameters);
+//$record = $stmt->fetch(PDO::FETCH_ASSOC);//expecting only one record
+
+
+//$record = $stmt->fetch(PDO::FETCH_ASSOC);//expecting only one record
+
+
+//print_r($namedParameters);
+
+//echo json_encode($record['Asin']);
 ?>
