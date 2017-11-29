64263fe7091d6771a51d58dbe83e36a074dc6104
eirkrw <ering-walters@csumb.edu>
2017-11-29 11:57:33 +0000

    done

--- cst336/homework/homework5/functions.php
+++ cst336/homework/homework5/functions.php
@@ -10,31 +10,37 @@ $sql = "INSERT INTO productHistory (Asin)
 $namedParameters = array();
 $namedParameters[':Asin'] =   $_GET['asin'] ;
 
-print_r($namedParameters);
+// print_r($namedParameters);
 
 $stmt = $conn->prepare($sql);
 $stmt->execute($namedParameters);
 
 $sql = "SELECT *
-        FROM tc_product
-        WHERE 1
-        AND Asin LIKE :Asin";
+        FROM productHistory
+        WHERE asin = :Asin";
+        //WHERE Asin = :Asin";
          
         
 $namedParameters = array();
-$namedParameters[':Asin'] = "%" . $_GET['asin'] . "%";
-
- //print_r($namedParameters);
-
-// $stmt = $conn->prepare($sql);
-// $stmt->execute($namedParameters);
-//$record = $stmt->fetch(PDO::FETCH_ASSOC);//expecting only one record
-
-
-//$record = $stmt->fetch(PDO::FETCH_ASSOC);//expecting only one record
-
+$namedParameters[':Asin'] = $_GET['asin'];
 
 //print_r($namedParameters);
 
-//echo json_encode($record['Asin']);
+ $stmt = $conn->prepare($sql);
+ $stmt->execute($namedParameters);
+$record = $stmt->fetchAll(PDO::FETCH_ASSOC);//expecting only one record
+
+
+//$record = $stmt->fetch(PDO::FETCH_ASSOC);//expecting only one record
+$count = 0;
+foreach($record as $records)
+{
+    $count++;
+echo json_encode(//$records['Asin']." ". 
+$records['time'] . "<br>");
+}
+echo "<p id='count'> Item searched for " . $count . " times. </p>";
+
+//print_r($namedParameters);
+
 ?>
