b236eaed8f91c28f143a98b5c8f822ccc3178c84
eirkrw <ering-walters@csumb.edu>
2017-12-01 21:51:17 +0000

    Finished with lab 10 requirements

--- cst336/labs/lab10/index.php
+++ cst336/labs/lab10/index.php
@@ -1,21 +1,66 @@
-<!Doctype html>
-<?php
-print_r($_FILES);
-echo $_FILES['myFile'][''];
-?>
+
+
+
+<!DOCTYPE html>
 <html>
-<head>
-    <title>Lab 10: Photo Gallery</title>
-</head>
-<body>
-    
-    <form method="POST" ectype="multipart/form-data">
+    <head>
+         <link rel="stylesheet" href="./styles.css"/>
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         
-        <input type="file" name="fileName"/>
+        <title> Lab 10: Photo Gallery </title>
+    </head>
+    <body>
+
+    <h2> Photo Gallery </h2>
+    <form method="POST" enctype="multipart/form-data"> 
+
+
+        <input type="file" name="myFile" /> 
         
-        <input type="submit" name="Upload File!"/>
-        
+        <input id="submit" type="submit" value="Upload File!" />
+
     </form>
     
-</body>
+    <script>
+    // $("img").click(function() {
+    //     $(".thumbnail").addClass("bigger");
+    //     $(".thumbnail").removeClass("thumbnail");
+    // })
+    </script>
+<?php
+
+  //print_r($_FILES);
+  //echo "File size " . $_FILES['myFile']['size'];
+//   move_uploaded_file($_FILES["myFile"]["tmp_name"], "gallery/" . $_FILES["myFile"]["name"] );
+//   {
+//   $files = scandir("gallery/", 1);
+//  // print_r($files);
+  
+ 
+//   }
+
+?>
+
+    </body>
 </html>
+<?php
+
+  $files = scandir("gallery/", 1);
+
+ 
+   if($_FILES['myFile']['size'] > 1000000)
+  {
+      echo "<h3> error : file size too big </h3>";
+  }
+  else{
+     // echo "<div> <img class='thumbnail' src='gallery/ " . $files[i] . "'width = '50' > </div>";
+     move_uploaded_file($_FILES["myFile"]["tmp_name"], "gallery/" . $_FILES['myFile']['name'] );
+  }
+  
+  for ($i = 0; $i < count($files) - 2; $i++) {
+     
+     echo " <div> <img class='thumbnail' src='gallery/" .   $files[$i] . "' width='50' ></div>";
+      
+  }
+?>
